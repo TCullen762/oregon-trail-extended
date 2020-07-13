7bf4dd9a3bb12fabff49b736a04f05ef26ede078
Timothy Cullen <cullen762@Timothys-MacBook-Air.local>
2020-07-13 11:38:14 -0400

    complete

--- main.js
+++ main.js
@@ -1,62 +1,86 @@
 
-
+//why is wagon undefined?
 
 class Traveler {
     constructor(name){
         this.name = name
         this.food  = 1
         this.isHealthy = true
+       
     }
     hunt(){
         
+       
     this.food  +=2
-    
- 
+        
+
+     
+
     }
     eat(){
-        this.food -= 1
+        
         if(this.food == 0){
-        this.isHealthy = false
+        this.isHealthy = false}
+        else{
+        this.food -= 1
+        }
 
     }
+    
+    }
 
-}
-}
 
 
 class Wagon {
-    constructor(){
-        this.capacity = 5
+    constructor(capacity){
+        this.capacity = capacity
         this.passengers = []
+       
 
     }
     getAvailableSeatCount(){ 
-        /**figure out array iterators */
-        for( let i =0; i < this.passengers.length; i ++){
-            return this.passengers.length 
-        }
+     
+        return this.capacity - this.passengers.length
+        
+            }
+        
+        
+        
+        
 
-        //Returns the number of empty seats, 
-        //determined by the capacity set when the wagon was created, 
-        //subtracted by the number of passengers currently on board.
-    }
-    join(traveler){
-        if(this.capacity < 5 ){
-            this.passengers.push(new Traveler(name))
-        }else{
-            //alert ('No room for you paddy, start walking!')
+        
+        
+
+    
+    
+    join(Traveler){
+        if(this.getAvailableSeatCount() > 0){
+            this.passengers.push(Traveler)
         }
         
     }
-    shouldQuarantine(traveler){
-            //if .some()doesnt work maybe try find?
-       this.passengers.some(this.isHealthy)
-           return true
+    shouldQuarantine(){
+       
+  
+           let healthCheck = this.passengers.some(traveler => traveler.isHealthy === false)
+       
+        return healthCheck
        }
-        //returns true if at least one unhealthy person is in the wagon, false if not
+
+ 
     
     totalFood(){
-        //returns total food of all passengers in the wagon
+            let totalFood = 0
+        for(let i = 0; i < this.passengers.length; i ++){
+            
+
+       totalFood += this.passengers[i].food  
+
+       
     }
+    return totalFood
 }
+
+}
+
 
--- tests.js
+++ tests.js
@@ -3,14 +3,13 @@ let wagon = new Wagon(2)
 let henrietta = new Traveler('Henrietta')
 let juan = new Traveler('Juan')
 let maude = new Traveler('Maude')
-let finian = new Traveler('finian')
-let yukino = new Traveler( 'Yukino')
+
 console.log(`Wagon Seat Count?: ${ wagon.getAvailableSeatCount() } – EXPECTED: 2. The wagon starts with 2 seats. We haven't added travelers to the wagon yet.`)
 wagon.join(henrietta)
 console.log(`Wagon Seat Count?: ${ wagon.getAvailableSeatCount() } – EXPECTED: 1. Henrietta just joined.`)
 wagon.join(juan)
-wagon.join(yukino)
-wagon.join(finian)
+
+
 wagon.join(maude)  // There is no room for her!
 console.log(`Wagon Seat Count?: ${ wagon.getAvailableSeatCount() } – EXPECTED: 0 – There is no room for Maude, but Juan was able to join.`)
 henrietta.hunt()   // Henrietta goes in search of food.
